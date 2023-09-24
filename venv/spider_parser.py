8c42d0d9cb472047aaf33410be62acd7ffe8b2ca
Lapteva Firyuza <laptevafiryuza@yandex.ru>
2023-09-24 19:56:26 +0500

    added URL handling

--- venv/spider_parser.py
+++ venv/spider_parser.py
@@ -1,38 +1,58 @@
 import threading
-from threading import Thread
 import requests
 from bs4 import BeautifulSoup
 import json
 
-class Parser(Thread):
-    """ Класс парсера """
- 
+class Parser(threading.Thread):
+    """Класс парсера"""
+
     def __init__(self, url, lock):
-        Thread.__init__(self)
+        threading.Thread.__init__(self)
         self.url = url
         self.lock = lock
 
     def run(self):
-        """ Запуск обработки """
-        response = requests.get(self.url)
-        with self.lock:
+        """Запуск обработки"""
+        try:
+            response = requests.get(self.url)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
                 soup = soup.find_all('h1', class_='headline-1')
-                print('Запрос выполнен успешно')
+                print(f'Запрос выполнен успешно для URL: {self.url}')
                 # Добавить заголовки в лист
                 all_article_titles = []
                 article_titles = [inner.text for inner in soup]
                 all_article_titles.extend(article_titles)
-                # записать все заголовки в файл
-                with open('spider_file.json', 'w', encoding='utf-8') as file:
-                    json.dump(all_article_titles, file, ensure_ascii=False, indent=4)
+                # Записать все заголовки в файл
+                with self.lock:
+                    with open('spider_file.json', 'w', encoding='utf-8') as file:
+                        json.dump(all_article_titles, file, ensure_ascii=False, indent=4)
+                        file.write('\n')  # записывать данные с новой строки
             else:
-                print('Произошла ошибка при выполнении запроса')
-    
+                print(f'Произошла ошибка при выполнении запроса для URL: {self.url}')
+        except requests.exceptions.RequestException as e:
+            print(f'Ошибка при запросе для URL: {self.url}, {e}')
 
-# Список URL-адресов для парсинга
-urls = ['https://holdings.panasonic', 'https://nic.panasonic']
+    # определяем пути для исходных и изменных данных URLs
+input_file_path = 'urls.txt'
+output_file_path = 'modified_urls.txt'
+
+# открываем исходный файл для чтения и изменный файл для записи
+with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
+    # Итеративно проходим по каждой строчки файла
+    for line in input_file:
+        # Добавляем "https://" в начале каждой строчки
+        modified_line = 'https://' + line.strip()
+        
+        # записываем изменения в modified_line
+        output_file.write(modified_line + '\n')
+
+#print(f"URLs have been modified and saved to {output_file_path}.")
+
+# Чтение URL адресов из файла
+with open('modified_urls.txt', 'r', encoding='utf-8') as file:
+    urls = [line.strip() for line in file]
+    print("Read URLs from file:", urls)
 
 # Создаем объект блокировки
 lock = threading.Lock()
@@ -43,7 +63,7 @@ for url in urls:
     parser = Parser(url, lock)
     parser.start()
     threads.append(parser)
- 
+
 # Ожидаем завершения всех обработчиков
 for parser in threads:
     parser.join()
