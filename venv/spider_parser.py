c02bdb83b9148afd19250bcc847a8988a516d800
Lapteva Firyuza <laptevafiryuza@yandex.ru>
2023-09-24 23:23:25 +0500

    add parser domen rugby

--- venv/spider_parser.py
+++ venv/spider_parser.py
@@ -3,8 +3,28 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+def url_mod(input_file_path, output_file_path):
+
+# открываем исходный файл для чтения и изменный файл для записи
+    with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
+    # Итеративно проходим по каждой строчки файла
+        for line in input_file:
+        # Добавляем "https://" в начале каждой строчки
+            modified_line = 'https://' + line.strip()
+
+        # записываем изменения в modified_line
+            output_file.write(modified_line + '\n')
+
+#print(f"URLs have been modified and saved to {output_file_path}.")
+
+url_mod('urls.txt', 'modified_urls.txt')
+# Чтение URL адресов из файла
+with open('modified_urls.txt', 'r', encoding='utf-8') as file:
+    urls = [line.strip() for line in file]
+#    print("Read URLs from file:", urls)
+
 class Parser(threading.Thread):
-    """Класс парсера"""
+   # Класс парсера
 
     def __init__(self, url, lock):
         threading.Thread.__init__(self)
@@ -12,48 +32,24 @@ class Parser(threading.Thread):
         self.lock = lock
 
     def run(self):
-        """Запуск обработки"""
+        scraped_data = []
+        #Запуск обработки
         try:
             response = requests.get(self.url)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
-                soup = soup.find_all('h1', class_='headline-1')
+                soup = soup.find_all('h1')
                 print(f'Запрос выполнен успешно для URL: {self.url}')
-                # Добавить заголовки в лист
-                all_article_titles = []
-                article_titles = [inner.text for inner in soup]
-                all_article_titles.extend(article_titles)
-                # Записать все заголовки в файл
-                with self.lock:
-                    with open('spider_file.json', 'w', encoding='utf-8') as file:
-                        json.dump(all_article_titles, file, ensure_ascii=False, indent=4)
-                        file.write('\n')  # записывать данные с новой строки
+                for h1 in soup:
+                    scraped_data.append({'url': url, 'content': h1.text})
+                    #Добавим спарсенные данные в spider_file.json
+                    with open('spider_file.json', 'a', encoding='utf-8') as file:
+                        json.dump(scraped_data, file, ensure_ascii=False, indent=4)
             else:
                 print(f'Произошла ошибка при выполнении запроса для URL: {self.url}')
         except requests.exceptions.RequestException as e:
             print(f'Ошибка при запросе для URL: {self.url}, {e}')
-
-    # определяем пути для исходных и изменных данных URLs
-input_file_path = 'urls.txt'
-output_file_path = 'modified_urls.txt'
-
-# открываем исходный файл для чтения и изменный файл для записи
-with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
-    # Итеративно проходим по каждой строчки файла
-    for line in input_file:
-        # Добавляем "https://" в начале каждой строчки
-        modified_line = 'https://' + line.strip()
-        
-        # записываем изменения в modified_line
-        output_file.write(modified_line + '\n')
-
-#print(f"URLs have been modified and saved to {output_file_path}.")
-
-# Чтение URL адресов из файла
-with open('modified_urls.txt', 'r', encoding='utf-8') as file:
-    urls = [line.strip() for line in file]
-    print("Read URLs from file:", urls)
-
+            
 # Создаем объект блокировки
 lock = threading.Lock()
 
