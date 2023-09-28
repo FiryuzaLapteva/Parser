71ef7a651a927808011cb76756f10d032dc665a6
Lapteva Firyuza <laptevafiryuza@yandex.ru>
2023-09-28 16:09:53 +0500

    add parser csv_file

--- venv/spider_parser.py
+++ venv/spider_parser.py
@@ -2,6 +2,7 @@ import threading
 import requests
 from bs4 import BeautifulSoup
 import json
+import csv
 
 def url_mod(input_file_path, output_file_path):
 
@@ -32,24 +33,32 @@ class Parser(threading.Thread):
         self.lock = lock
 
     def run(self):
-        scraped_data = []
-        #Запуск обработки
         try:
             response = requests.get(self.url)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
                 soup = soup.find_all('h1')
                 print(f'Запрос выполнен успешно для URL: {self.url}')
-                for h1 in soup:
-                    scraped_data.append({'url': url, 'content': h1.text})
-                    #Добавим спарсенные данные в spider_file.json
-                    with open('spider_file.json', 'a', encoding='utf-8') as file:
-                        json.dump(scraped_data, file, ensure_ascii=False, indent=4)
+                spider_file = 'parsed_data.csv'
+                with open(spider_file, mode='a', newline='', encoding='utf-8') as file:
+                    csv_writer = csv.writer(file)
+                    # Пройтись по распарсенным данным и записать каждую строку
+                    for h1 in soup:
+                        row = [self.url, h1.text]  # Определить данные для каждой строки
+                        csv_writer.writerow(row)
             else:
                 print(f'Произошла ошибка при выполнении запроса для URL: {self.url}')
         except requests.exceptions.RequestException as e:
             print(f'Ошибка при запросе для URL: {self.url}, {e}')
-            
+
+# Создайте строку заголовка для CSV-файла (если необходимо)
+header = ['url', 'content']
+
+# Откройте CSV-файл в режиме записи и запишите заголовок
+with open('parsed_data.csv', mode='w', newline='', encoding='utf-8') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(header)
+          
 # Создаем объект блокировки
 lock = threading.Lock()
 
